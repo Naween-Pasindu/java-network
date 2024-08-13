19436bee64460a133f8cc59a6587eda7505fd359
Naween Pasindu <Naween-Pasindu@users.noreply.github.com>
2024-08-13 21:22:46 +0530

    Buffer

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
 import java.awt.*;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws IOException {
         InetAddress[] Address = InetAddress.getAllByName("www.google.com");
         System.out.println(Arrays.toString(Address));
+        try{
+            URL url = new URL("https://www.google.com");
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+            bufferedReader.close();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
