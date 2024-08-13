eb65b8478ed0f7e4a5672175a87aa2a4c2ff7509
Naween Pasindu <Naween-Pasindu@users.noreply.github.com>
2024-08-13 21:32:03 +0530

    channel

--- Main.java
+++ Main.java
@@ -1,12 +1,34 @@
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+        try{
+            URL url = new URL("https://www.google.com");
+            URLConnection urlConnection = url.openConnection();
+            InputStream inputStream = urlConnection.getInputStream();
+            ReadableByteChannel readableByteChannel = Channels.newChannel(inputStream);
+            ByteBuffer buffer = ByteBuffer.allocate(64);
+            String line;
+            while (readableByteChannel.read(buffer)  > 0){
+                System.out.println(new String(buffer.array()));
+                buffer.clear();
+            }
+            readableByteChannel.close();
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
         InetAddress[] Address = InetAddress.getAllByName("www.google.com");
         System.out.println(Arrays.toString(Address));
         try{
